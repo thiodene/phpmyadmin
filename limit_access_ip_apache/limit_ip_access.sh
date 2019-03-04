a2f2efa22fa5ff6173d247ab7fccd777a7b793dc
Serge Ayissi <ayissi.serge@gmail.com>
2019-03-04 10:14:21 -0500

    Create limit_ip_access.sh

---
+++ limit_access_ip_apache/limit_ip_access.sh
@@ -0,0 +1,16 @@
+# To limit the access of the users IP to PHPMyAdmin from the URL do the following:
+
+# Edit apache.conf
+sudo vi /etc/phpmyadmin/apache.conf
+
+# Add the lines
+<Directory /usr/share/phpmyadmin>
+
+    Options FollowSymLinks
+    DirectoryIndex index.php
+
+    Order Allow,Deny
+    Allow from 127.0.0.1
+    Allow from 68.24.9.0/24
+
+    <IfModule mod_php5.c>
